528a6e321e4b201c6f0af0d0a6bade5864312d2c
Zuo <tomasz_wozny@users.noreply.github.com>
2022-01-12 12:04:09 +0100

    db config

---
+++ Pasta-API/src/test/java/pl/pjatk/PastaAPI/PastaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.PastaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
